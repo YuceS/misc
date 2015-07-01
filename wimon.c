YuceS/misc

commit 712a5086d099d032133b217e0b0637bd652893e0

Note timestamp of probe-request with non-empty SSID

wimon.c
@@ -1459,10 +1459,10 @@ static int process_frame(const unsigned char *bssid, const unsigned char *da,
 		memset(sta->current_ssid, 0, SSID_LEN);
 	}
 	else if(mgmt_st == MGMT_PROBE_REQ && strlen(ssid)) {
-		sprintf(msg, FMT_PREFIX "%s probing for SSID: %s",
+		sprintf(msg, FMT_PREFIX "%s probing for SSID '%s' at: %s",
 			mactoa(sta->mac), signal, nfltoa(sta->flags),
 			sta->current_ssid, sta->flags & NFL_AP? "AP": "Station",
-			ssid);
+			ssid, ctime(&tv->tv_sec));
 		log_node_message(sta, msg);
 	}
 

commit b2a20c22df08a94dc4133f3328e7a89d7ef5c30e

Fix newline issue introduced in commit 712a5086

wimon.c
@@ -1484,6 +1484,7 @@ static int process_frame(const unsigned char *bssid, const unsigned char *da,
 			nfltoa(sta->flags), sta->current_ssid,
 			sta->flags & NFL_AP? "AP": "Station",
 			ssid, ctime(&tv->tv_sec));
+		msg[strlen(msg) - 1] = 0;
 		log_node_message(sta, msg);
 	}
 